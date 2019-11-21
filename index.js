58b344f0489f8716f40bc3fc8488494a695b537e
Michal Púchy <puchy.michal@gmail.com>
2019-11-21 22:18:04 +0100

    Phonebook backend - step3

--- index.js
+++ index.js
@@ -61,6 +61,16 @@ app.get('/info', (req, res) => {
 	`)
 })
 
+app.get('/api/persons/:id', (req, res) => {
+	const id = Number(req.params.id)
+	const person = persons.find(p => p.id === id)
+	if(person) {
+		res.json(person)
+	} else {
+		res.status(400).end()
+	}
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
 	console.log(`Server is running on port ${PORT}`)
