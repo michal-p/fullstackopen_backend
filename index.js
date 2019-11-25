76c06a5e7371ea8b821bc8d9e161277d08962ce3
Michal Púchy <puchy.michal@gmail.com>
2019-11-25 10:27:49 +0100

    Phonebook backend - Heroku setup

--- index.js
+++ index.js
@@ -127,7 +127,7 @@ const unknownEndpoint = (request, response) => {
 }
 app.use(unknownEndpoint)
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001//Heroku port or 3001
 app.listen(PORT, () => {
 	console.log(`Server is running on port ${PORT}`)
 })
