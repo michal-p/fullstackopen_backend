35c6e2b3af8f185160b9595f9282c81071e314ec
Michal Púchy <puchy.michal@gmail.com>
2019-12-20 14:34:31 +0100

    Eslint configurated

--- models/person.js
+++ models/person.js
@@ -18,15 +18,16 @@ mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
 const phonebookSchema = new mongoose.Schema({
 	name: {
     type: String,
-    minlength: 5,
+    minlength: 3,
     unique: true,
 		required: [true, 'User name min length 5 characters required']
 	},
 	number: {
     type: Number,
+    unique: true,
     validate: {
       validator: function (v) {
-        return String(v).length === 1
+        return String(v).length >= 8
       },
       message: props => `${props.value} is not a valid phone number`
     },
